ea20fb02214a549bae45ce30d180630ced8089fc
ShayShahar <shayscal90@gmail.com>
2016-04-13 22:13:38 +0300

    initial prototype added

---
+++ Development/Prototype/src/client/GHealthClient.java
@@ -0,0 +1,18 @@
+package client;
+
+import ocsf.client.*;
+
+public class GHealthClient extends AbstractClient{
+
+	public GHealthClient(String host, int port) {
+		super(host, port);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void handleMessageFromServer(Object msg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ Development/Prototype/src/server/GHealthServer.java
@@ -0,0 +1,18 @@
+package server;
+
+import ocsf.server.*;
+
+public class GHealthServer extends AbstractServer {
+
+	public GHealthServer(int port) {
+		super(port);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void handleMessageFromClient(Object msg, ConnectionToClient client) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
