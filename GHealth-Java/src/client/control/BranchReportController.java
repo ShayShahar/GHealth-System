d904d0e9bbcde67856d4f16b1fc1e3615fa0fa89
yakir1114 <yakir1114@gmail.com>
2016-05-09 17:47:55 +0300

    Branch Manage UI+fxml +controller

--- GHealth-Java/src/client/control/BranchReportController.java
+++ GHealth-Java/src/client/control/BranchReportController.java
@@ -1,5 +1,20 @@
 package client.control;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class BranchReportController{
 	
+	//FXML variables
+	@FXML private Button WeeklyBtn;
+	@FXML private Button MonthlyBtn;
+
+	
+public void onWeeklyReportButtonClick(ActionEvent event){
+	
+
+	
+}
+
 }
