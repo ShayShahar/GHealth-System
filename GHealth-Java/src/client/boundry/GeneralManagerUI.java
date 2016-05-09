21bbeadc7951d8b1eb19086ed3da4b3fa543c9a0
yakir1114 <yakir1114@gmail.com>
2016-05-09 21:25:32 +0300

    General UI + branchReport controller

--- GHealth-Java/src/client/boundry/GeneralManagerUI.java
+++ GHealth-Java/src/client/boundry/GeneralManagerUI.java
@@ -1,35 +1,102 @@
 package client.boundry;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 
 import client.control.ClientConnectionController;
 import client.interfaces.IUi;
+import common.entity.Request;
+import common.enums.Command;
 import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 import javafx.util.Pair;
 
 public class GeneralManagerUI implements IUi{
-
+	
+	private static Stage mainStage;
+	
 	@Override
 	public void hideWindow() {
-		// TODO Auto-generated method stub
-		
+		Platform.runLater(new Runnable() {
+
+			@Override
+			public void run() {
+					mainStage.hide();
+			}
+			});		
 	}
+
 
 	@Override
 	public void showWindow() {
-		// TODO Auto-generated method stub
-		
+		Platform.runLater(new Runnable() {
+
+			@Override
+			public void run() {
+					mainStage.show();	
+			}
+			});		
 	}
 
 	@Override
 	public void displayUserWindow() {
-		// TODO Auto-generated method stub
-		
-	}
+					
+			Platform.runLater(new Runnable() {
+			
+				@Override
+				public void run() {
+					URL url_fxml = LoginUI.class.getResource("/client/boundry/fxml/GeneralManagerUI.fxml");
+				  FXMLLoader fxmlLoader = new FXMLLoader(url_fxml);
+				  Parent root;
+					try {
+								root = (Parent) fxmlLoader.load();
+					      Stage stage = new Stage();
+					      stage.setTitle("General Manager Menu");
+								URL url_32 = LoginUI.class.getResource("/img/icon_32.png");
+								stage.getIcons().add(new Image(url_32.toString()));
+					      Scene scene = new Scene(root);
+					      scene.getStylesheets().add("client/boundry/css/Style.css");
+					      stage.setScene(scene); 
+					      stage.setResizable(false);
+					      
+					      stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+					            public void handle(WindowEvent we) {
+					            	
+					        		ArrayList<String> username = new ArrayList<String>();
+					        		username.add(ClientConnectionController.clientConnect.userName);
+					        	
+					        		Request request = new Request(Command.LOGOUT, username);
 
+					        		try {
+					        			ClientConnectionController.clientConnect.sendToServer(request);
+					        		} catch (IOException e) {
+					        			e.printStackTrace();
+					        		}
+					            
+					           }
+					     });
+					      stage.show();	
+					      mainStage = stage;
+					    
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+			
+				}
+			});		
+		}
+	
 	@Override
 	public void displayErrorMessage(String title, String information) {
 		Platform.runLater(new Runnable() {
@@ -40,7 +107,6 @@ public class GeneralManagerUI implements IUi{
 				Dialog<Pair<String, String>> dialog = new Dialog<>();
 				dialog.setTitle("Error Message");
 				dialog.setHeaderText(title);
-				dialog.getDialogPane().getStylesheets().add("client/boundry/css/JMetroLightTheme.css");
 				dialog.setContentText(information);
 				dialog.setGraphic(new ImageView(url.toString()));
 				dialog.getDialogPane().getButtonTypes().addAll(ButtonType.OK);
@@ -60,7 +126,6 @@ public class GeneralManagerUI implements IUi{
 				dialog.setTitle("Information");
 				dialog.setHeaderText(title);
 				dialog.setContentText(information);
-				dialog.getDialogPane().getStylesheets().add("client/boundry/css/JMetroLightTheme.css");
 				dialog.setGraphic(new ImageView(url.toString()));
 				dialog.getDialogPane().getButtonTypes().addAll(ButtonType.OK);
 				dialog.showAndWait();
