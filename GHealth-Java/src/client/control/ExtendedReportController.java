044fa842341db4418fdc112a4ce5ca38a0dfcf12
ShayShahar <shayscal90@gmail.com>
2016-05-12 10:01:03 +0300

    GeneralManagerUI start

--- GHealth-Java/src/client/control/ExtendedReportController.java
+++ GHealth-Java/src/client/control/ExtendedReportController.java
@@ -1,5 +1,42 @@
 package client.control;
 
-public class ExtendedReportController{
+import java.io.IOException;
+import java.util.ArrayList;
+
+import client.interfaces.IController;
+import common.entity.Reply;
+import common.entity.Request;
+import common.enums.Command;
+import javafx.event.ActionEvent;
+
+public class ExtendedReportController implements IController{
+
+	
+	
+	
+	
+	
+	public void onLogoutButtonClick(ActionEvent event){
+		
+		ArrayList<String> username = new ArrayList<String>();
+		username.add(UserController.getUser());
+	
+		Request request = new Request(Command.LOGOUT, username);
+
+		try {
+			ClientConnectionController.clientConnect.controller = this;
+			ClientConnectionController.clientConnect.sendToServer(request);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+	@Override
+	public void handleReply(Reply reply) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
