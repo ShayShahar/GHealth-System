e1984fa6c12def3f7edf18f75d4c4f0df4360b3d
ShayShahar <shayscal90@gmail.com>
2016-05-18 16:56:48 +0300

    Request & Reply javaDoc comments

--- GHealth-Java/src/common/entity/Reply.java
+++ GHealth-Java/src/common/entity/Reply.java
@@ -3,7 +3,12 @@ package common.entity;
 import common.enums.*;
 import java.io.Serializable;
 
-
+/**
+ * General reply class used to handle replies from the server
+ * Class implements Serializable
+ * @author shays
+ *
+ */
 public class Reply implements Serializable{
 
 		private static final long serialVersionUID = 1L;
@@ -11,19 +16,33 @@ public class Reply implements Serializable{
 		private final Object result;
     private final Command command;
 
+    /**
+     * Reply class constructor
+     * @param result
+     * @param command
+     */
     public Reply(Object result, Command command) {
     	this.result = result;
     	this.command = command;
 
     }
     
+    /**
+     * Get result property from Reply instance
+     * @return Object
+     */
+    
     public Object getResult() {
     	return this.result;
     }
 
+    /**
+     * Get command property from Reply instance
+     * @return Enum Command
+     */
+    
     public Command getCommand() {
     	return this.command;
     }
-    
     
 }
