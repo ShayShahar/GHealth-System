f9ab7a5f403b83b970fe22964d6e73ee24c69318
ShayShahar <shayscal90@gmail.com>
2016-05-22 20:50:26 +0300

    tryyy

--- GHealth-Java/src/server/control/DBController.java
+++ GHealth-Java/src/server/control/DBController.java
@@ -7,7 +7,7 @@ import server.control.database.*;
 
 public class DBController {
 
-	
+	//
 	public static Object processRequest(Request request, Connection connection){
 
 		switch (request.getCommand()){
