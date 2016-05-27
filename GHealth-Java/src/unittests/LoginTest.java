d53fb126ce080b23331de4c6bc125b8feb9ac766
yakir1114 <yakir1114@gmail.com>
2016-05-27 15:14:31 +0300

    MediaFileTest + LoginTest

---
+++ GHealth-Java/src/unittests/LoginTest.java
@@ -0,0 +1,33 @@
+package unittests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LoginTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void test1() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void test2() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void test3() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void test4() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void test5() {
+		fail("Not yet implemented");
+	}
+}
---
+++ GHealth-Java/src/unittests/MediaFileTest.java
@@ -0,0 +1,34 @@
+package unittests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MediaFileTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void test1() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void test2() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void test3() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void test4() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void test5() {
+		fail("Not yet implemented");
+	}
+}
